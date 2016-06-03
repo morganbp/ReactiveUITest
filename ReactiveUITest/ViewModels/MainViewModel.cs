c33f531d263e12f8429e6ccea7dd58c55ec2508f
Morgan Pettersen <morgibp@gmail.com>
2016-06-03 10:51:45 +0200

    Initial Commit

---
+++ ReactiveUITest/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReactiveUITest
+{
+	public class MainViewModel
+	{
+		public MainViewModel ()
+		{
+		}
+	}
+}
+
---
+++ eSmart.Test.ReactiveUI.Droid/Screens/FragmentActivityBase.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace eSmart.Test.ReactiveUI.Droid
+{
+	[Activity (Label = "FragmentActivityBase")]			
+	public class FragmentActivityBase : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Create your application here
+		}
+	}
+}
+
