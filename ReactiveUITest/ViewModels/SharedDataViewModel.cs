3a6f07dc8c2418004da404ad1d7f07bef02f9232
Morgan Pettersen <morgibp@gmail.com>
2016-06-06 08:26:18 +0200

    leftover previous commit

--- ReactiveUITest/ViewModels/SharedDataViewModel.cs
+++ ReactiveUITest/ViewModels/SharedDataViewModel.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using ReactiveUI;
 
 namespace ReactiveUITest
 {
-	public class SharedDataViewModel
+	public class SharedDataViewModel : ReactiveObject
 	{
+		private static SharedDataViewModel _instance;
+		private static object _synchLock = new object();
+		private object _initSynch = new object();
+		private bool _initialized = false;
+
 		public SharedDataViewModel ()
 		{
+		}
+
+		public static SharedDataViewModel Instance 
+		{
+			get
+			{
+				lock (_synchLock) {
+					if (_instance == null) {
+						_instance = new SharedDataViewModel ();
+					}
+					return _instance;
+				}
+			}
+		}
+
+		public void EnsureInitialized()
+		{
+			lock(_initSynch){
+				if (!_initialized) {
+					Initialize ();
+					_initialized = true;
+				}		
+			}
+		}
+
+		public void Initialize(){
+			
 		}
 	}
 }
